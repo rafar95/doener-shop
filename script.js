d9e95752c6fa23f56d9790ebd43fdb218729a027
rafar95 <rafael.reichelt@protonmail.com>
2024-03-05 20:11:23 +0100

    Step 3 Update 1

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("start_pic-container");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
