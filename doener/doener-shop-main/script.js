2fc0126df7a66cb4b4d671ba26801016e1c36a82
rafar95 <159924019+rafar95@users.noreply.github.com>
2024-03-23 01:07:04 +0100

    Add files via upload

---
+++ doener/doener-shop-main/script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("start_pic-container");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+$('h3 span:first-child').after("<span class=\"dots\"> </span>");
